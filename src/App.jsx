b9870393de89ac2d320bdfdce322503b62436566
vinfreecs <89305950+vinfreecs@users.noreply.github.com>
2023-12-25 08:46:03 +0530

    Fixed font-size

--- src/App.jsx
+++ src/App.jsx
@@ -231,7 +231,7 @@ function App() {
           <a href="mailto:vinayrbs001@gmail.com">vinayrbs001@gmail.com</a>
         </p>
         <p>
-        <a href="https://www.freepnglogos.com/images/pokemon-logo-png-1447.html">pokemon symbol logo</a>
+        <a href="https://www.freepnglogos.com/images/pokemon-logo-png-1447.html">logo</a>
         </p>
       </footer>
     </>
